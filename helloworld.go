2344a621b563a5d170f5990a156836455678e71a
Matteo Conti <56634851+matteo-conti-97@users.noreply.github.com>
2023-02-04 19:16:23 +0100

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
-	"encoding/hex"
 )
 
 func main() {
-	var succIdHex,_=hex.Decode([]byte("AASD08924X"))
-	var currIdHex,_=hex.Decode([]byte("AASD08924Z"))
-	fmt.Println("La distanza é %d",hex.Encode(succIdHex[]-currIdHex[]))
+	var set = make(map[string]int)
+	set["11"] = 1
+	set["44"] = 4
+	set["22"] = 2
+	set["33"] = 3
+	set["55"] = 5
+	set["66"] = 6
+	fmt.Println(set)
+	delete(set, "11")
+	fmt.Println(set["Bob"])
+	for _, v := range set {
+		fmt.Println(v)
+	}
 }
